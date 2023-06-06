584da86bdaff76871c6ffcc6b02f569f3d1bf407
iurylippo <iuryvieira.lippo@gmail.com>
2023-06-06 00:20:57 -0300

    feat: setup tailwind colors

--- frontend/sia_localiza_app/src/components/layout/sidebar.tsx
+++ frontend/sia_localiza_app/src/components/layout/sidebar.tsx
@@ -26,7 +26,7 @@ const Sidebar = ({ open, navItems = defaultNavItems, setOpen }: Props) => {
     <div
       className={classNames({
         'flex flex-col justify-between': true, // layout
-        'bg-indigo-700 text-zinc-50': true, // colors
+        'bg-primary-500 text-zinc-50': true, // colors
         'md:w-full md:sticky md:top-16 md:z-0 top-0 z-20 fixed': true, // positioning
         'md:h-[calc(100vh_-_64px)] h-full w-[300px]': true, // for height and width
         'transition-transform .3s ease-in-out md:-translate-x-0': true, // animations
@@ -42,7 +42,7 @@ const Sidebar = ({ open, navItems = defaultNavItems, setOpen }: Props) => {
               <Link key={index} href={item.href}>
                 <li
                   className={classNames({
-                    'text-indigo-100 hover:bg-indigo-900': true, // colors
+                    'text-indigo-100 hover:bg-title': true, // colors
                     'flex gap-4 items-center ': true, // layout
                     'transition-colors duration-300': true, // animation
                     'rounded-md p-2 mx-2': true, // self style
--- frontend/sia_localiza_app/tailwind.config.js
+++ frontend/sia_localiza_app/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,6 +9,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        sm: '640px',
+        // => @media (min-width: 640px) { ... }
+
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -17,6 +35,31 @@ module.exports = {
       },
       gridTemplateRows: {
         header: '64px auto', // 👈 for the navbar layout. adds grid-rows-header class
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      boxShadow: {
+        red: '0 0 5pt 2pt #ffd6d6',
+      },
+      colors: {
+        ...colors,
+        primary: {
+          200: 'var(--color-primary-200)',
+          300: 'var(--color-primary-300)',
+          400: 'var(--color-primary-400)',
+          500: 'var(--color-primary-500)',
+          600: 'var(--color-primary-600)',
+        },
+        secondary: {
+          200: 'var(--color-secondary-200)',
+          300: 'var(--color-secondary-300)',
+          400: 'var(--color-secondary-400)',
+          500: 'var(--color-secondary-500)',
+          600: 'var(--color-secondary-600)',
+        },
+        title: 'var(--text-primary)',
       },
     },
   },
